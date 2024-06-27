6d0e134ad827ec3be0c4465539a3aa3a4674e22b
Rochettoro <130759213+Rochettoro@users.noreply.github.com>
2024-06-27 11:43:22 +0200

    Update DsBot.java

--- DsBot.java
+++ DsBot.java
@@ -9,6 +9,9 @@ public class DsBot {
     DsBot(String token){
         this.token = token;
         jda = JDABuilder.createDefault(token).build();
+        CommandManager cmdmanager = new CommandManager();
+        cmdmanager.add(new Play());
+        jda.addEventListener(cmdmanager);
         ConsoleToServer cts = new ConsoleToServer();
     }
 
