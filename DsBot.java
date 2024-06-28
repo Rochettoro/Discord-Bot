2716221e8a81c832256d2d229fb5b658c299cdf1
Rochettoro <130759213+Rochettoro@users.noreply.github.com>
2024-06-28 16:01:51 +0200

    Update DsBot.java

--- DsBot.java
+++ DsBot.java
@@ -10,14 +10,17 @@ public class DsBot {
         this.token = token;
         jda = JDABuilder.createDefault(token).build();
         CommandManager cmdManager = new CommandManager();
+        cmdManager.add(new GithubFileDownloader());
         cmdManager.add(new ForwardMessage());
         cmdManager.add(new Play());
         cmdManager.add(new Stop());
         cmdManager.add(new Skip());
         cmdManager.add(new Queue());
         cmdManager.add(new Now());
+
         McScanner scan = new McScanner();
         scan.getOptions();
+
         cmdManager.add(scan);
         jda.addEventListener(cmdManager);
         ConsoleToServer cts = new ConsoleToServer();
